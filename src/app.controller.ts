32e93c1bed2af559eec8492804c2234b373d5d3d
loyboy <loyboy606@gmail.com>
2024-07-21 20:25:09 +0100

    21st July

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  getHelloIndex(): string {
+    return 'welcome to nestjs';
+  }
+
+  @Get('onlyuser')
   async getHello(@User() user): Promise<string> {
     return await this.appService.getHello(user.id);
   }
